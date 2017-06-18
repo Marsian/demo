078f324877c3814d272379d1ed1b9ccea5a6c02a
Yanxi Wang <mars@Yanxis-MacBook-Pro.local>
2017-06-18 12:25:17 -0600

    Script try

--- postinstall.sh
+++
@@ -1,7 +0,0 @@
-HOME=$OPENSHIFT_REPO_DIR npm uninstall babel-core || npm uninstall babel-core
-HOME=$OPENSHIFT_REPO_DIR npm install babel-core || npm install babel-core
-HOME=$OPENSHIFT_REPO_DIR npm install unpipe || npm install unpipe
-HOME=$OPENSHIFT_REPO_DIR npm install type-is || npm install type-is
-HOME=$OPENSHIFT_REPO_DIR npm install vary || npm install vary
-HOME=$OPENSHIFT_REPO_DIR npm install utils-merge || npm install utils-merge
-HOME=$OPENSHIFT_REPO_DIR npm run build || npm run build
