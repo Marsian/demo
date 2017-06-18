52f07318750da7e2ce70cc3ba609db34ff91d13f
Yanxi Wang <mars@Yanxis-MacBook-Pro.local>
2017-06-18 12:32:12 -0600

    fix post install script

--- postInstall.sh
+++ postInstall.sh
@@ -1,11 +1,11 @@
-if [ -d \"$OPENSHIFT_REPO_DIR\" ]
+if [ -d $OPENSHIFT_REPO_DIR ]
 then
-    export HOME=\"$OPENSHIFT_REPO_DIR\"/..;
-    HOME=$OPENSHIFT_REPO_DIR npm uninstall babel-core || npm uninstall babel-core
-    HOME=$OPENSHIFT_REPO_DIR npm install babel-core || npm install babel-core
-    HOME=$OPENSHIFT_REPO_DIR npm install unpipe || npm install unpipe
-    HOME=$OPENSHIFT_REPO_DIR npm install type-is || npm install type-is
-    HOME=$OPENSHIFT_REPO_DIR npm install vary || npm install vary
-    HOME=$OPENSHIFT_REPO_DIR npm install utils-merge || npm install utils-merge
-    HOME=$OPENSHIFT_REPO_DIR npm run build || npm run build
+    export HOME=$OPENSHIFT_REPO_DIR;
+    npm uninstall babel-core
+    npm install babel-core
+    npm install unpipe
+    npm install type-is
+    npm install vary
+    npm install utils-merge
+    npm run build
 fi
