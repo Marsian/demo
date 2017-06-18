e54489013f7d8eec5005d86409dac5720d1c61be
Yanxi Wang <mars@Yanxis-MacBook-Pro.local>
2017-06-18 12:06:15 -0600

    Try post install script

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,7 @@
-HOME=$OPENSHIFT_REPO_DIR npm update || npm update
-HOME=$OPENSHIFT_REPO_DIR npm build || npm build
+HOME=$OPENSHIFT_REPO_DIR npm uninstall babel-core || npm uninstall babel-core
+HOME=$OPENSHIFT_REPO_DIR npm install babel-core || npm install babel-core
+HOME=$OPENSHIFT_REPO_DIR npm install unpipe || npm install unpipe
+HOME=$OPENSHIFT_REPO_DIR npm install type-is || npm install type-is
+HOME=$OPENSHIFT_REPO_DIR npm install vary || npm install vary
+HOME=$OPENSHIFT_REPO_DIR npm install utils-merge || npm install utils-merge
+HOME=$OPENSHIFT_REPO_DIR npm run build || npm run build
